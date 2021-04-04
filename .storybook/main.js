99325a6bc66246be3fd0489bbe92b0754aacf58e
Katalina Okano <git@kat.cafe>
2021-04-03 20:34:23 -0400

    webpack5 no repro

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,13 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../stories-1/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories-2/**/*.stories.@(js|jsx|ts|tsx)",
-    "../Intro.stories.mdx", // XXX: Comment this to fix reload loop. Repros with *.stories.mdx too.
+    "../intro.stories.mdx", // XXX: Comment this to fix reload loop. Repros with *.stories.mdx too.
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
 };
