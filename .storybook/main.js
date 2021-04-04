5d83f38815911ad8f6b3d41e3db23605c3a3a1ce
Katalina Okano <git@kat.cafe>
2021-04-03 20:11:28 -0400

    fix react deps and comment repro location

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     "../stories-1/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories-2/**/*.stories.@(js|jsx|ts|tsx)",
-    "../*.stories.mdx",
+    "../*.stories.mdx", // XXX: Comment this to fix reload loop
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
