9b5681b998121e78f2494e87b2e78a3049df8839
Katalina Okano <git@kat.cafe>
2021-04-03 20:16:35 -0400

    scope in repro to not include globs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     "../stories-1/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories-2/**/*.stories.@(js|jsx|ts|tsx)",
-    "../*.stories.mdx", // XXX: Comment this to fix reload loop
+    "../Intro.stories.mdx", // XXX: Comment this to fix reload loop. Repros with *.stories.mdx too.
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
